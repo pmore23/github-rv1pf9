cca8782587239c390b15b0f806cf99c3cb1c7e31
pmore23 <23priyankamore@gmail.com>
2022-08-23 12:05:50 +0530

    event binding with button click

--- kitchen-stories/src/app/home-page/home-page.component.ts
+++ kitchen-stories/src/app/home-page/home-page.component.ts
@@ -19,6 +19,7 @@ export class HomePageComponent implements OnInit {
   ];
   btnDisabledFlag = true; // property binding
   buttonColor = 'btnColor';
+  buttonText = true;
   imgSourceUrl =
     'https://cdn.pixabay.com/photo/2018/10/11/12/31/black-cat-3739702_1280.jpg';
 
@@ -42,4 +43,8 @@ export class HomePageComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  btnClicked(event: MouseEvent) {
+    console.log('button was clicked : ', event);
+  }
 }
